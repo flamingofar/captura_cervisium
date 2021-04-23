9985c2ada3c346c4bdced572b195c38daefd910b
Malte Skjoldager <malteskjoldager@gmail.com>
2021-04-23 12:32:01 +0200

    Random delay, random speed og sidevogn

--- js/app.js
+++ js/app.js
@@ -15,13 +15,17 @@ function startGame() {
 	console.log(promille);
 	vandAnimationer();
 	beerAnimationer();
+	sidevognAnimationer();
 }
 // Vand Funktioner
 function vandAnimationer() {
 	let randomPos = randomTal();
+	let randomDel = randomDelay();
+	let randomSpd = randomSpeed();
+	// Random Position, delay og rotation animation
 	document
 		.querySelector("#vand_container")
-		.classList.add("rotate_arm", randomPos);
+		.classList.add("rotate_arm", randomPos, randomDel, randomSpd);
 	document
 		.querySelector("#vand_container")
 		.addEventListener("mousedown", clickHandlerVand);
@@ -52,27 +56,35 @@ function clickHandlerVand() {
 		"url('assets/ui_elementer/barometer/barometer_2.svg')";
 }
 function resetVand() {
+	document
+		.querySelector("#vand_container")
+		.removeEventListener("animationiteration", resetVand);
 	let randomPos = randomTal();
+	let randomDel = randomDelay();
+	let randomSpd = randomSpeed();
 
-	document.querySelector("#vand_container").classList.remove("rotate_arm");
-	document.querySelector("#vand_sprite").classList.remove("blow_out");
-	document.querySelector("#vand_container").classList.remove("pause");
-	document.querySelector("#vand_container").classList.remove("pos6");
+	document.querySelector("#vand_container").classList = "";
+	document.querySelector("#vand_sprite").classList = "";
 
+	document.querySelector("#vand_container").classList.add(randomDel, randomSpd);
+	document.querySelector("#vand_container").offsetHeight;
 	document
 		.querySelector("#vand_container")
-		.classList.add("rotate_arm", randomPos);
+		.classList.add(randomPos, "rotate_arm");
 	document
 		.querySelector("#vand_container")
-		.addEventListener("mousedown", clickHandlerVand);
+		.addEventListener("animationiteration", resetVand);
 }
 
 // Fadøl Funktioner
 function beerAnimationer() {
 	let randomPos = randomTal();
+	let randomDel = randomDelay();
+	let randomSpd = randomSpeed();
+	// Random Position, delay og rotation animation
 	document
 		.querySelector("#beer_container")
-		.classList.add("rotate_arm", randomPos);
+		.classList.add("rotate_arm", randomPos, randomDel, randomSpd);
 	// Fadøl Animationer
 	document
 		.querySelector("#beer_container")
@@ -88,8 +100,8 @@ function clickHandlerBeer() {
 	document
 		.querySelector("#beer_container")
 		.removeEventListener("mousedown", clickHandlerBeer);
-	document.querySelector("#beer_sprite").classList.add("fade_out");
 	document.querySelector("#beer_container").classList.add("pause");
+	document.querySelector("#beer_sprite").classList.add("fade_out");
 	document.querySelector("#beer_container").classList.remove("rotate_arm");
 
 	// +1 Promille, udskriv point
@@ -103,23 +115,110 @@ function clickHandlerBeer() {
 		"url('assets/ui_elementer/barometer/barometer_10.svg')";
 }
 function resetBeer() {
-	let randomPos = randomTal();
-
-	document.querySelector("#beer_container").className = "";
-	document.querySelector("#beer_sprite").className = "";
-	// document.querySelector("#beer_container").classList.remove("pause");
-	// document.querySelector("#beer_container").classList.remove("pos4");
 	document
 		.querySelector("#beer_container")
-		.classList.add("rotate_arm", randomPos);
+		.removeEventListener("animationiteration", resetBeer);
+	let randomPos = randomTal();
+	let randomDel = randomDelay();
+	let randomSpd = randomSpeed();
+
+	document.querySelector("#beer_container").classList = "";
+	document.querySelector("#beer_sprite").classList = "";
+
+	document.querySelector("#beer_container").classList.add(randomDel, randomSpd);
+	document.querySelector("#beer_container").offsetHeight;
+	document
+		.querySelector("#beer_container")
+		.classList.add(randomPos, "rotate_arm");
 
 	document
 		.querySelector("#beer_container")
 		.addEventListener("mousedown", clickHandlerBeer);
+	document
+		.querySelector("#beer_container")
+		.addEventListener("animationiteration", resetBeer);
+	console.log(randomPos);
 }
 
+// Sidevogn Funktioner
+function sidevognAnimationer() {
+	let randomPos = randomTal();
+	let randomDel = randomDelay();
+	let randomSpd = randomSpeed();
+	// Random Position, delay og rotation animation
+	document
+		.querySelector("#sidevogn_container")
+		.classList.add("rotate_arm", randomPos, randomDel, randomSpd);
+	// Fadøl Animationer
+	document
+		.querySelector("#sidevogn_container")
+		.addEventListener("mousedown", clickHandlersidevogn);
+	document
+		.querySelector("#sidevogn_sprite")
+		.addEventListener("animationend", resetsidevogn);
+	document
+		.querySelector("#sidevogn_container")
+		.addEventListener("animationiteration", resetsidevogn);
+}
+function clickHandlersidevogn() {
+	document
+		.querySelector("#sidevogn_container")
+		.removeEventListener("mousedown", clickHandlersidevogn);
+	document.querySelector("#sidevogn_container").classList.add("pause");
+	document.querySelector("#sidevogn_sprite").classList.add("fade_out");
+	document.querySelector("#sidevogn_container").classList.remove("rotate_arm");
+
+	// +1 Promille, udskriv point
+	promille += 2;
+
+	document.querySelector("#energy_board_count").innerHTML =
+		promille + "&permil;";
+
+	// Skift barometerbillede
+	document.querySelector("#energy_board").style.backgroundImage =
+		"url('assets/ui_elementer/barometer/barometer_7.svg')";
+}
+function resetsidevogn() {
+	document
+		.querySelector("#sidevogn_container")
+		.removeEventListener("animationiteration", resetsidevogn);
+	let randomPos = randomTal();
+	let randomDel = randomDelay();
+	let randomSpd = randomSpeed();
+
+	document.querySelector("#sidevogn_container").classList = "";
+	document.querySelector("#sidevogn_sprite").classList = "";
+
+	document
+		.querySelector("#sidevogn_container")
+		.classList.add(randomDel, randomSpd);
+	document.querySelector("#sidevogn_container").offsetHeight;
+	document
+		.querySelector("#sidevogn_container")
+		.classList.add(randomPos, "rotate_arm");
+
+	document
+		.querySelector("#sidevogn_container")
+		.addEventListener("mousedown", clickHandlersidevogn);
+	document
+		.querySelector("#sidevogn_container")
+		.addEventListener("animationiteration", resetsidevogn);
+	console.log(randomPos);
+}
+
+/*--------------------------------------------------- Model Funktioner ---------------------------------------------------*/
 function randomTal() {
 	let randomTal = Math.floor(Math.random() * 10) + 1;
 	let randomPos = "pos" + randomTal;
 	return randomPos;
+}
+function randomDelay() {
+	let randomTal = Math.floor(Math.random() * 4) + 1;
+	let randomDelay = "delay" + randomTal;
+	return randomDelay;
+}
+function randomSpeed() {
+	let randomTal = Math.floor(Math.random() * 4) + 1;
+	let randomSpeed = "speed" + randomTal;
+	return randomSpeed;
 }
